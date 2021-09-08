4c8821f9a9f6d0da3ca6f253527514103d522977
saurabh kulkarni <kulkarnis009@gmail.com>
2021-09-08 11:02:08 +0530

    keyvault_Key_encryption_decryption

--- KeyVaultAppConsoleApp1/KeyVaultAppConsoleApp1/Program.cs
+++ KeyVaultAppConsoleApp1/KeyVaultAppConsoleApp1/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Azure.Identity;
+using Azure.Security.KeyVault.Keys;
+using Azure.Security.KeyVault.Keys.Cryptography;
 using Azure.Security.KeyVault.Secrets;
 using System;
+using System.Text;
 
 namespace KeyVaultAppConsoleApp1
 {
@@ -11,17 +14,40 @@ namespace KeyVaultAppConsoleApp1
         private static string clintsecret = "~r6yU8nJJHxk1Wyb~-9eIasm1~N285r3.Q";
 
         private static string keyvaultUrl = "https://az204keyvault1.vault.azure.net/";
-        private static string secret_name = "dbpassword";
+        //private static string secret_name = "dbpassword";
+        private static string key_name = "dbpassword2";
+        private static string key_to_encrypt = "This text need to encrypted";
+
         static void Main(string[] args)
         {
 
             ClientSecretCredential clientSecretCredential = new ClientSecretCredential(tenantid,clientid,clintsecret);
 
-            SecretClient secretClient = new SecretClient(new Uri(keyvaultUrl),clientSecretCredential);
+            //SecretClient secretClient = new SecretClient(new Uri(keyvaultUrl),clientSecretCredential);
 
-            var secretvar = secretClient.GetSecret(secret_name);
-            
-            Console.WriteLine($"Db password is : {secretvar.Value.Value}");
+            KeyClient keyClient = new KeyClient(new Uri(keyvaultUrl), clientSecretCredential);
+
+            //var secretvar = secretClient.GetSecret(secret_name);
+
+            var _key = keyClient.GetKey(key_name);
+
+            var crypto_client = new CryptographyClient(_key.Value.Id, clientSecretCredential);
+
+            byte[] text_to_bytes = Encoding.UTF8.GetBytes(key_to_encrypt);
+
+            //Encryption
+            EncryptResult result = crypto_client.Encrypt(EncryptionAlgorithm.RsaOaep, text_to_bytes);
+
+            Console.WriteLine($"Encrypted text is {Convert.ToBase64String(result.Ciphertext)}");
+            //Console.WriteLine($"Db password is : {secretvar.Value.Value}");
+
+
+            //Decryption
+            byte[] byte_to_text = result.Ciphertext;
+
+            DecryptResult decryptResult = crypto_client.Decrypt(EncryptionAlgorithm.RsaOaep, byte_to_text);
+
+            Console.WriteLine($"Decrypted text is: {Encoding.UTF8.GetString(decryptResult.Plaintext)}");
 
             Console.ReadKey();
         }
