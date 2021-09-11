6cf9337509fdb561f56b3e2a8ad59ef81b7a6fe7
saurabh kulkarni <kulkarnis009@gmail.com>
2021-09-11 21:09:57 +0530

    Managed identity code for keyvault

---
+++ KeyVaultAppConsole2/KeyVaultAppConsole2/Program.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using System;
+
+namespace KeyVaultAppConsole2
+{
+    class Program
+    {
+        private static string keyvalut_url = "https://az204keyvault3.vault.azure.net/";
+        private static string secret_name = "password2";
+
+        static void Main(string[] args)
+        {
+
+            TokenCredential credential = new DefaultAzureCredential();
+
+            SecretClient secretClient = new SecretClient(new Uri(keyvalut_url), credential);
+
+            var secret=secretClient.GetSecret(secret_name);
+
+            Console.WriteLine($"The secret is {secret.Value.Value}");
+        }
+    }
+}
